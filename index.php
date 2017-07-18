e28fdec132862d15e3293d40fe88a34e612f5c74
Rama Patel <me@rexsdev.com>
2017-07-18 10:21:11 -0400

    Favicon & stuff

--- index.php
+++ index.php
@@ -1,1 +1,49 @@
-Something new is coming soon!
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Rexs Development &amp; Design </title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+		<meta name="keywords" content="rexs, rexs123, rexs183, rexsdev, develpment, web, design, web design, web development, system, linux, mysql, design development, app, dev, graphics, flat, material">
+		<meta name="description" content="Rexs Development &amp; Design; freelance web design and development out of Toronto, Canada..">
+
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="https://www.rexsdev.com">
+		<meta property="og:site_name" content="Rexs Development">
+		<meta property="og:description" content="Rexs Development &amp; Design a web design and development company.">
+		<meta property="og:locale" content="en-US">
+
+		<meta name="twitter:domain" content="rexsdev.com">
+		<meta name="twitter:url" content="https://www.rexsdev.com">
+		<meta name="twitter:description" content="Professional Web Design Development Company">
+		<meta name="twitter:site" content="@rexs183">
+
+		<meta name="robots" content="index,follow,noodp,noydir">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha256-NuCn4IvuZXdBaFKJOAcsU2Q3ZpwbdFisd5dux4jkQ5w=" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha256-916EbMg70RQy9LHiGkXzG8hSg9EdNy97GazNG/aiY1w=" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+		<link href="https://rexsdev.com/assets/css/responsive.min.css" rel="stylesheet">
+		<link href="https://rexsdev.com/assets/css/style.min.css" rel="stylesheet">
+		<link href="https://rexsdev.com/assets/css/rexsdev.min.css" rel="stylesheet">
+		<link href="https://rexsdev.com/assets/css/image-server.min.css" rel="stylesheet">
+
+		<link rel="apple-touch-icon" sizes="57x57" href="https://rexsdev.com/assets/img/favicon/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="https://rexsdev.com/assets/img/favicon/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="https://rexsdev.com/assets/img/favicon/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="https://rexsdev.com/assets/img/favicon/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="https://rexsdev.com/assets/img/favicon/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="https://rexsdev.com/assets/img/favicon/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="https://rexsdev.com/assets/img/favicon/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="https://rexsdev.com/assets/img/favicon/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="https://rexsdev.com/assets/img/favicon/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192" href="https://rexsdev.com/assets/img/favicon/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="https://rexsdev.com/assets/img/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="https://rexsdev.com/assets/img/favicon/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="https://rexsdev.com/assets/img/favicon/favicon-16x16.png">
+		<link rel="manifest" href="https://rexsdev.com/assets/img/favicon/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="https://rexsdev.com/assets/img/favicon/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha256-U5ZEeKfGNOja007MMD3YBI0A3OSZOQbeG6z2f2Y0hu8=" crossorigin="anonymous"></script>
+</head>
+<body>
