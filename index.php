d78e5de066e8b0c43d13889336226f73ab3c0bb8
Rama Patel <me@rexsdev.com>
2017-07-18 10:23:03 -0400

    Sleep

--- index.php
+++ index.php
@@ -47,3 +47,8 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha256-U5ZEeKfGNOja007MMD3YBI0A3OSZOQbeG6z2f2Y0hu8=" crossorigin="anonymous"></script>
 </head>
 <body>
+  <div class="container">
+    <h1>Coming Soon</h1>
+  </div>
+</body>
+</html>
